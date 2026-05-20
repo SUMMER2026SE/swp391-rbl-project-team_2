4ee6fd7a356faefbf55bdb2ce069c8586964d377
Dung Phuong <pdung4367@gmail.com>
2026-05-20 14:23:00 +0700

    feat(landlord): implement AI System Monitoring page based on Figma

--- client/src/features/landlord/index.js
+++ client/src/features/landlord/index.js
@@ -5,5 +5,6 @@ export { default as AddNewPropertyPage } from './pages/AddNewPropertyPage';
 export { default as DepositManagementPage } from './pages/DepositManagementPage';
 export { default as SystemLogsPage } from './pages/SystemLogsPage';
 export { default as LandlordProfilePage } from './pages/LandlordProfilePage';
+export { default as AISystemMonitoringPage } from './pages/AISystemMonitoringPage';
 export { default as MessagesPage } from './pages/MessagesPage';
 export { default as LandlordNotificationsPage } from './pages/LandlordNotificationsPage';
